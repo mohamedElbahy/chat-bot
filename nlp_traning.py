791c9f8562e9d0409813a930a97a15325ea2ae0f
mohamedElbahy <38901833+mohamedElbahy@users.noreply.github.com>
2018-05-07 09:17:50 +0200

    Add files via upload

---
+++ nlp_traning.py
@@ -0,0 +1,41 @@
+import os
+import random
+import spacy
+keys=[]
+trian_dic=dict()
+for files in os.listdir('C:/Users/bahy\Downloads\chatterbot-corpus-master\chatterbot_corpus\data\english'):
+    data = open('C:/Users/bahy\Downloads\chatterbot-corpus-master\chatterbot_corpus\data\english/' + files,'r').readlines()
+    for i in range(0,(len(data)-1)):
+        x=data[i]
+        if x.startswith("- -"):
+            y=x.replace('-','')
+            y = y.replace('\n', '')
+            if not y in keys:
+                keys.append(y)
+            while i < len(data)-1 and not data[i+1].startswith("- -") :
+                i+=1
+                x = data[i].replace('-', '')
+                x = x.replace('\n', '')
+                if y in trian_dic:
+                    trian_dic[y].append(x)
+                else:
+                  trian_dic[y] = [x]
+nlp = spacy.load('en')
+def replay(mess):
+
+    doc = nlp(mess)
+    key=""
+    max=-1.1
+    for x in keys:
+            sim=doc.similarity(nlp(x))
+            if sim > max:
+                max=sim
+                key=x
+
+    #print(key)
+    return random.choice(trian_dic[key])
+
+message = input('you :')
+
+print('smsm :' + replay(message))
+
